4877309c43c9f2ac9431e54bdea1ec8554dbd34d
David Yang <yadewe@live.com>
2015-12-02 15:01:12 +0800

    Init Utility Project
    add Entity helper

---
+++ YDW/YDW.Utility/Entity/EntityHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace YDW.Utility.Entity
+{
+    public class EntityHelper
+    {
+        /// <summary>
+        /// 类型转换，复制同名的字段值
+        /// </summary>
+        /// <typeparam name="TConvert"></typeparam>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public static TConvert ConvertTo<TConvert>(this object entity) where TConvert : new()
+        {
+            var convertProperties = TypeDescriptor.GetProperties(typeof(TConvert)).Cast<PropertyDescriptor>();
+            var entityProperties = TypeDescriptor.GetProperties(entity).Cast<PropertyDescriptor>();
+
+            var convert = new TConvert();
+
+            foreach (var entityProperty in entityProperties)
+            {
+                var property = entityProperty;
+                var convertProperty = convertProperties.FirstOrDefault(prop => prop.Name == property.Name);
+                if (convertProperty != null)
+                {
+                    convertProperty.SetValue(convert, Convert.ChangeType(entityProperty.GetValue(entity), convertProperty.PropertyType));
+                }
+            }
+
+            return convert;
+        }
+    }
+}
