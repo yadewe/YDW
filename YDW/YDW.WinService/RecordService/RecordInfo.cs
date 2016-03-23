7fffc16ad1e19c009e211d1db83e9e547f141006
David Yang <yadewe@live.com>
2016-03-23 09:26:11 +0800

    split file, ajust reader
    split RecordFile to RecordFile/RecordInfo/RecordReader.
    write char '_' to replace blank.

---
+++ YDW/YDW.WinService/RecordService/RecordInfo.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YDW.WinService
+{
+    public class RecordInfo
+    {
+        public bool IsRemark { get; set; }
+        public DateTime? Start { get; set; }
+        public DateTime? Stop { get; set; }
+
+        public string Remark { get; set; }
+
+        public TimeSpan? TotalLate
+        {
+            get
+            {
+                if (Start.HasValue)
+                {
+                    var begin = new DateTime(Start.Value.Year, Start.Value.Month, Start.Value.Day, 9, 0, 0);
+                    if (Start > begin)
+                        return Start.Value - begin;
+                    return null;
+                }
+                return null;
+            }
+        }
+        public TimeSpan? TotalMonthLate
+        {
+            get
+            {
+                return GetTotalMonthLate(this);
+            }
+        }
+
+        public string TotalMonthLateStr
+        {
+            get
+            {
+                if (!TotalMonthLate.HasValue)
+                    return null;
+                TimeSpan ts = TotalMonthLate.Value;
+                return String.Format("{0} {1}:{2}:{3}", ts.Days, ts.Hours, ts.Minutes, ts.Seconds);
+            }
+
+        }
+
+        public TimeSpan? GetTotalMonthLate(RecordInfo info)
+        {
+            if (info.PreRecordInfo == null || info.PreRecordInfo.Month != info.Month)
+                return info.TotalLate;
+            TimeSpan lastMonthSpan = GetTotalMonthLate(info.PreRecordInfo) ?? TimeSpan.Zero;
+            return lastMonthSpan + (info.TotalLate ?? TimeSpan.Zero);
+        }
+
+        public DateTime? Month
+        {
+            get
+            {
+                if (Start.HasValue)
+                    return new DateTime(Start.Value.Year, Start.Value.Month, 1);
+                if (Stop.HasValue)
+                    return new DateTime(Stop.Value.Year, Stop.Value.Month, 1);
+                return null;
+            }
+        }
+
+        public TimeSpan? Span
+        {
+            get
+            {
+                if (Start.HasValue && Stop.HasValue && Start < Stop)
+                    return Stop.Value - Start.Value;
+                return null;
+            }
+        }
+        public string SpanStr
+        {
+            get
+            {
+                if (Span.HasValue)
+                {
+                    var ts = Span.Value;
+                    return String.Format("{0} {1}:{2}:{3}", ts.Days, ts.Hours, ts.Minutes, ts.Seconds);
+                }
+                return null;
+            }
+        }
+        public RecordInfo PreRecordInfo { get; set; }
+        public TimeSpan? PreSpan
+        {
+            get
+            {
+                if (PreRecordInfo == null)
+                    return null;
+                return PreRecordInfo.Span;
+            }
+        }
+
+        private static readonly RecordReader[] _readers =  {
+            new Reader3(),
+            //new Reader5(),
+            //new Reader4(),
+            new Reader2(),
+            new Reader1() // v1.0.1
+        };
+
+
+
+        public static bool IsRecordInfo(string infoString)
+        {
+            return _readers.Any(p => p.IsMatch(infoString));
+        }
+
+        public RecordInfo()
+        {
+        }
+
+        public RecordInfo(string infoString)
+        {
+            if (!IsRecordInfo(infoString))
+            {
+                Remark = infoString;
+                IsRemark = true;
+            }
+            else
+            {
+                IsRemark = false;
+                var reader = _readers.First(p => p.IsMatch(infoString));
+
+                var info = reader.Read(infoString);
+                Start = info.Start;
+                Stop = info.Stop;
+            }
+        }
+
+        public override string ToString()
+        {
+            if (IsRemark)
+                return Remark;
+            else
+                return _readers.First().Write(this);
+        }
+    }
+}
---
+++ YDW/YDW.WinService/RecordService/RecordReader.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace YDW.WinService
+{
+    public abstract class RecordReader
+    {
+        protected string ReadPattern;
+        protected string WritePattern;
+
+        public abstract RecordInfo Read(string infoString);
+        public abstract string Write(RecordInfo info);
+        public bool IsMatch(string infoString)
+        {
+            return Regex.IsMatch(infoString, ReadPattern);
+        }
+    }
+
+    // v1.0.1
+    public class Reader1 : RecordReader
+    {
+        public Reader1()
+        {
+            ReadPattern = "--RI--([^-]*)----([^-]*)----([^-]*)----([^-]*)----([^-]*)----$";
+            WritePattern = "--RI--{0}----{1}----{2}----{3}----{4}----";
+        }
+
+        public override RecordInfo Read(string infoString)
+        {
+            RecordInfo info = new RecordInfo();
+            var match = Regex.Match(infoString, ReadPattern);
+            info.Start = DateTime.Parse(match.Groups[1].Value + " " + match.Groups[2].Value);
+            info.Stop = DateTime.Parse(match.Groups[1].Value + " " + match.Groups[3].Value);
+
+            return info;
+        }
+
+        public override string Write(RecordInfo info)
+        {
+            return string.Format(WritePattern,
+                    info.Start.HasValue ? info.Start.Value.ToString("yyyy/MM/dd") : null,
+                    info.Start.HasValue ? info.Start.Value.ToString("HH:mm:ss") : null,
+                    info.Stop.HasValue ? info.Stop.Value.ToString("HH:mm:ss") : null,
+                    info.SpanStr,
+                    info.TotalMonthLateStr
+                    );
+        }
+
+    }
+
+    // v1.0.2
+    public class Reader2 : RecordReader
+    {
+        public Reader2()
+        {
+            ReadPattern = "--RI--([^-]*)----([^-]*)----([^-]*)----([^-]*)----$";
+            WritePattern = "--RI--{0}----{1}----{2}----{3}----";
+        }
+
+        public override RecordInfo Read(string infoString)
+        {
+            RecordInfo info = new RecordInfo();
+            var match = Regex.Match(infoString, ReadPattern);
+            if (!string.IsNullOrEmpty(match.Groups[1].Value))
+                info.Start = DateTime.Parse(match.Groups[1].Value);
+            if (!string.IsNullOrEmpty(match.Groups[2].Value))
+                info.Stop = DateTime.Parse(match.Groups[2].Value);
+
+            return info;
+        }
+
+        public override string Write(RecordInfo info)
+        {
+            string timeFormat = "yyyy/MM/dd HH:mm:ss";
+            return string.Format(WritePattern,
+                    info.Start.HasValue ? info.Start.Value.ToString(timeFormat) : null,
+                    info.Stop.HasValue ? info.Stop.Value.ToString(timeFormat) : null,
+                    info.SpanStr,
+                    info.TotalMonthLateStr
+                    );
+        }
+    }
+    // v1.0.3
+    public class Reader3 : RecordReader
+    {
+        protected char repeatChar = '_';
+        public Reader3()
+        {
+            ReadPattern = "--RI3--([^-]*)----([^-]*)----([^-]*)----([^-]*)----$";
+            WritePattern = "--RI3--{0}----{1}----{2}----{3}----";
+        }
+
+        public override RecordInfo Read(string infoString)
+        {
+            RecordInfo info = new RecordInfo();
+            var match = Regex.Match(infoString, ReadPattern);
+            if (!string.IsNullOrEmpty(match.Groups[1].Value) &&
+                !string.IsNullOrEmpty(match.Groups[1].Value.Replace(repeatChar.ToString(), "")))
+                info.Start = DateTime.Parse(match.Groups[1].Value);
+            if (!string.IsNullOrEmpty(match.Groups[2].Value) &&
+                !string.IsNullOrEmpty(match.Groups[2].Value.Replace(repeatChar.ToString(), "")))
+                info.Stop = DateTime.Parse(match.Groups[2].Value);
+
+            return info;
+        }
+
+        public override string Write(RecordInfo info)
+        {
+            string timeFormat = "yyyy/MM/dd HH:mm:ss";
+            return string.Format(WritePattern,
+                    info.Start.HasValue ? info.Start.Value.ToString(timeFormat) : "".PadLeft(timeFormat.Length + 3, repeatChar),
+                    info.Stop.HasValue ? info.Stop.Value.ToString(timeFormat) : "".PadLeft(timeFormat.Length + 3, repeatChar),
+                    info.SpanStr,
+                    info.TotalMonthLateStr
+                    );
+        }
+    }
+    // v1.0.4
+    public class Reader4 : Reader3
+    {
+        public Reader4()
+        {
+            repeatChar = 'O';
+            ReadPattern = "--RI4--([^-]*)----([^-]*)----([^-]*)----([^-]*)----$";
+            WritePattern = "--RI4--{0}----{1}----{2}----{3}----";
+        }
+    }
+    public class Reader5 : Reader3
+    {
+        public Reader5()
+        {
+            repeatChar = 'o';
+            ReadPattern = "--RI5--([^-]*)----([^-]*)----([^-]*)----([^-]*)----$";
+            WritePattern = "--RI5--{0}----{1}----{2}----{3}----";
+        }
+    }
+}
